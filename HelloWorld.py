1b65f93ef5498d2ace70f7f15f5e251231787ed9
Julian Osmer <julian.osmer@outlook.de>
2021-11-07 01:57:35 +0100

    update

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,16 +1,5 @@
 import SimpleITK as sitk
 
-fixedImage = sitk.ReadImage('fixedImage.png')
-movingImage = sitk.ReadImage('movingImage.png')
-parameterMap = sitk.GetDefaultParameterMap('translation')
-
-elastixImageFilter = sitk.ElastixImageFilter()
-elastixImageFilter.SetFixedImage(fixedImage)
-elastixImageFilter.SetMovingImage(movingImage)
-elastixImageFilter.SetParameterMap(parameterMap)
-elastixImageFilter.Execute()
-
-resultImage = elastixImageFilter.GetResultImage()
-transformParameterMap = elastixImageFilter.GetTransformParameterMap()
+resultImage = sitk.Elastix(sitk.ReadImage("fixedImage.png"),sitk.ReadImage("movingImage.png"), "translation")
 
 sitk.Show(resultImage, title="Hello World: Python", debugOn=True)
