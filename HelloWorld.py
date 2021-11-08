49d72264dadb342ca61592bdf8164e077ad5b667
Julian Osmer <julian.osmer@outlook.de>
2021-11-08 17:00:05 +0100

    update

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,5 +1,6 @@
 import SimpleITK as sitk
 
-resultImage = sitk.Elastix(sitk.ReadImage("fixedImage.png"),sitk.ReadImage("movingImage.png"), "translation")
+resultImage = sitk.Elastix(sitk.ReadImage("sae/he_phh3/Bio5_HE.png", sitk.sitkInt8),
+                           sitk.ReadImage("sae/he_phh3/Bio5_PHH3.png", sitk.sitkInt8), "translation")
 
 sitk.Show(resultImage, title="Hello World: Python", debugOn=True)
